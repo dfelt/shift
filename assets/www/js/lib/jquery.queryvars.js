b3f779ef76cecfcb6c9fb953c0843a8aabf3ac9b
David Felty <davfelty@gmail.com>
2013-07-10 14:54:56 -0400

    Added plugin to get url query string vars

---
+++ assets/www/js/lib/jquery.queryvars.js
@@ -0,0 +1,21 @@
+/**
+ * $.getUrlVars - Get url search parameters and parse into an object.
+ */
+(function($) {
+var re = /([^&=]+)=?([^&]*)/g;
+var decodeRE = /\+/g;  // Regex for replacing addition symbol with a space
+var decode = function (str) {return decodeURIComponent( str.replace(decodeRE, " ") );};
+$.getQueryVars = function() {
+    var query = window.location.href.split('?')[1] || '';
+    var params = {}, e;
+    while ( (e = re.exec(query)) ) { 
+        var k = decode( e[1] ), v = decode( e[2] );
+        if (k.substring(k.length - 2) === '[]') {
+            k = k.substring(0, k.length - 2);
+            (params[k] || (params[k] = [])).push(v);
+        }
+        else { params[k] = v; }
+    }
+    return params;
+};
+}(jQuery));
